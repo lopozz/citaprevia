9a5238aeb915fef7470b9b2a2460cd340ee58d6e
Lorenzo Pozzi <lpozzi@pcopernico.it>
2025-02-28 17:04:57 +0100

    found first cue

--- main_cue.py
+++ main_cue.py
@@ -5,64 +5,29 @@ import time
 import logging
 import datetime
 
-import undetected_chromedriver as uc
-
 from fake_useragent import UserAgent
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from utils import update_json_with_current_time, random_delay, send_discord_message
-
-
-def create_driver(headless=True):
-    options = uc.ChromeOptions()
-
-    if headless:
-        options.add_argument("--headless=new")
-        options.add_argument("--disable-gpu")
-
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--ignore-ssl-errors")
-    options.add_argument("--allow-insecure-localhost")
-    options.add_argument("--disable-web-security")
-
-    # Mimic regular browser behavior
-    options.add_argument("--start-maximized")
-
-    # adding argument to disable the AutomationControlled flag
-    options.add_argument("--disable-blink-features=AutomationControlled")
-
-    # options.add_experimental_option("useAutomatadd_experimental_optionionExtension", False)
-
-    # Use history data
-    home_directory = os.path.expanduser(
-        "~"
-    )  # Expands '~' to the full home directory path
-    user_data_dir = f"{home_directory}/.config/google-chrome/Default"
-    options.add_argument(f"--user-data-dir={user_data_dir}")
-    # options.add_argument("--user-data-dir=/tmp/selenium-profile")
-    # options.add_argument("--user-data-dir=~/.config/google-chrome/Default")
-
-    options.add_argument(f"--user-agent={ua.random}")
-
-    options.add_argument("accept-language=es-ES,es;q=0.9,en;q=0.8")
-
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-
-    return uc.Chrome(
-        headless=headless,
-        options=options,
-        driver_executable_path="/opt/chromedriver-linux64/chromedriver",
-        browser_executable_path="/usr/bin/google-chrome",
-        version_main=131,
-    )
+from src.utils import (
+    random_delay,
+    send_discord_message,
+    update_df_with_appointment,
+    create_driver,
+    is_within_time_ranges,
+)
 
 
 WEBHOOK_URL = os.environ["WEBHOOK_URL"]
 MIN = 2
 MAX = 4
+CITY = "Valencia"
+OFICINA = None  # "CNP-COMISARIA DE BAILEN, Bailen, 9, VALENCIA"
+URL = "https://sede.administracionespublicas.gob.es/pagina/index/directorio/icpplus"
+DEFAULT_INTERVAL = 20
+TARGETED_INTERVAL = 5
+TARGET_TIMES = [("9:20", "9:40")]
 
 logging.basicConfig(
     level=logging.INFO,
@@ -72,10 +37,9 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-ua = UserAgent()
 
 
-def main(headless=True):
+def check_cue(headless=False):
     driver = create_driver(headless)
     wait = WebDriverWait(driver, 10)
 
@@ -99,62 +63,63 @@ def main(headless=True):
         "Network.setExtraHTTPHeaders",
         {
             "headers": {
-                "User-Agent": ua.random,
-                "Referer": "https://icp.administracionelectronica.gob.es/icpplus/index.html",
+                "User-Agent": UserAgent().random,
+                "Referer": URL,
             }
         },
     )
     driver.implicitly_wait(0.25)
 
-    cookies = driver.get_cookies()
-    for cookie in cookies:
-        driver.add_cookie(cookie)
+    # cookies = driver.get_cookies()
+    # for cookie in cookies:
+    #     driver.add_cookie(cookie)
+    driver.delete_all_cookies()
 
     try:
         logger.info("Initializing Chrome driver...")
 
-        url = "https://icp.administracionelectronica.gob.es/icpplus/index.html"
-        driver.get(url)
+        driver.get(URL)
         random_delay(MIN, MAX)
 
-        # wait.until(lambda d: d.execute_script("return document.readyState") == "complete")
-
-        logger.info("Starting CUE procedure...")
-
-        random_delay(MIN, MAX)
-        # Select province
-        # element = wait.until(EC.presence_of_element_located((By.ID, 'form')))
-        element = driver.find_element(By.ID, "form")
-        select = Select(element)
-        select.select_by_visible_text("Valencia")
-        random_delay(MIN, MAX)
-
-        element = driver.find_element(By.ID, "btnAceptar")
+        element = wait.until(EC.element_to_be_clickable((By.ID, "submit")))
         element.click()
 
-        # element = wait.until(EC.element_to_be_clickable((By.ID, "btnAceptar")))
-        # driver.execute_script("arguments[0].scrollIntoView(true);", element)
+        random_delay(MIN, MAX)
+
+        # Select province
+        element = wait.until(EC.presence_of_element_located((By.ID, "form")))
+        select = Select(element)
+        select.select_by_visible_text(CITY)
+        random_delay(MIN, MAX)
+        element = wait.until(EC.element_to_be_clickable((By.ID, "btnAceptar")))
+        element.click()
+
+        # Select oficina
         random_delay(MIN, MAX)
         logger.info("CUE procedure in progress...")
 
-        # Select comisaria
-
-        # time.sleep(random.uniform(2, 5))
-        random_delay(MIN, MAX)
-        element = wait.until(EC.presence_of_element_located((By.ID, "sede")))
-        select = Select(element)
-        select.select_by_visible_text("CNP-COMISARIA DE BAILEN, Bailen, 9, VALENCIA")
-        random_delay(MIN, MAX)
-
-        element = wait.until(EC.presence_of_element_located((By.ID, "tramiteGrupo[0]")))
-        # toggle_option = driver.find_element(By.ID, '')
-        select = Select(element)
-        select.select_by_visible_text(
-            "POLICIA-CERTIFICADO DE REGISTRO DE CIUDADANO DE LA U.E."
-        )
+        if OFICINA:
+            element = wait.until(EC.presence_of_element_located((By.ID, "sede")))
+            select = Select(element)
+            select.select_by_visible_text(OFICINA)
+            random_delay(MIN, MAX)
+            element = wait.until(
+                EC.presence_of_element_located((By.ID, "tramiteGrupo[0]"))
+            )
+            select = Select(element)
+            select.select_by_visible_text(
+                "POLICIA-CERTIFICADO DE REGISTRO DE CIUDADANO DE LA U.E."
+            )
+        else:
+            element = wait.until(
+                EC.presence_of_element_located((By.ID, "tramiteGrupo[1]"))
+            )
+            select = Select(element)
+            select.select_by_visible_text(
+                "POLICIA-CERTIFICADO DE REGISTRO DE CIUDADANO DE LA U.E."
+            )
         random_delay(MIN, MAX)
         element = wait.until(EC.element_to_be_clickable((By.ID, "btnAceptar")))
-        # driver.execute_script("arguments[0].scrollIntoView(true);", element)
         element.click()
 
         random_delay(MIN, MAX)
@@ -162,9 +127,6 @@ def main(headless=True):
         element.click()
 
         # Insert personal data
-        # random_delay(MIN, MAX)
-        # element = wait.until(EC.element_to_be_clickable((By.ID, "btnAceptar")))
-        # element.click()
         random_delay(MIN, MAX)
         element = wait.until(
             EC.element_to_be_clickable(
@@ -176,66 +138,61 @@ def main(headless=True):
         element = wait.until(
             EC.presence_of_element_located((By.ID, "txtIdCitado"))
         ).send_keys("YB6548687")
-        # element = driver.find_element(By.ID, "txtIdCitado").send_keys("Z2800743S")
-        # element = WebDriverWait(driver, 10).until(
-        #     EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/main/div/div/section/div[2]/form/div/div/div[1]/div[2]/div/div/div[2]/input"))
-        # )
-        # element.send_keys("Z2800743S")
-        # pasaporte_field = driver.find_element(By.ID, "txtIdCitado").send_keys("YB6548687")
-        # for char in "YB6548687":
-        #     pasaporte_field.send_keys(char)
-        #     time.sleep(random.uniform(0.1, 0.3))
-        # time.sleep(random.uniform(2, 5))
         element = wait.until(
             EC.presence_of_element_located((By.ID, "txtDesCitado"))
         ).send_keys("LORENZO POZZI")
 
-        # name_field = driver.find_element(By.ID, "txtDesCitado").send_keys("LORENZO POZZI")
-        # for char in "LORENZO POZZI":
-        #     name_field.send_keys(char)
-        #     time.sleep(random.uniform(0.1, 0.3))
-
         random_delay(MIN, MAX)
         element = wait.until(EC.element_to_be_clickable((By.ID, "btnEnviar")))
         element.click()
-        # button = driver.find_element(By.ID, "btnEnviar").click()
 
         random_delay(MIN, MAX)
         element = wait.until(EC.element_to_be_clickable((By.ID, "btnEnviar")))
         element.click()
 
-        # button = driver.find_element(By.ID, "btnEnviar").click()
-        current_url = driver.current_url
-
-        elements = driver.find_elements(
-            By.XPATH,
-            "/html/body/div[1]/div/main/div/div[2]/section/div[2]/form/div[1]/p",
+        WebDriverWait(driver, 20).until(
+            lambda d: d.execute_script("return document.readyState") == "complete"
         )
 
-        if elements:
-            send_discord_message(WEBHOOK_URL, current_url, logger)
-            driver.save_screenshot("screenshot_yes.png")
-            with open("page_source_success.html", "w", encoding="utf-8") as f:
+        if "En este momento no hay citas disponibles" in driver.page_source:
+            current_day = datetime.datetime.today().strftime("%A")
+            logger.info("❌ No citas...")
+        elif (
+            "Su sesión ha caducado por permanecer demasiado tiempo inactiva"
+            in driver.page_source
+        ):
+            current_day = datetime.datetime.today().strftime("%A")
+            logger.error("⚠️ Error inactive session...")
+        else:
+            logger.info("✅ A cita was found!")
+            with open(
+                f"page_source_success_{str(datetime.datetime.now()).replace(' ', '_')}.html",
+                "w",
+                encoding="utf-8",
+            ) as f:
                 f.write(driver.page_source)
 
-            update_json_with_current_time(
-                "./citaprevia/data.json"
-            )
-            logger.info("A cita was found!")
-        else:
-            current_day = datetime.datetime.today().strftime("%A")
-            logger.info(
-                f"No citas on {current_day} at {datetime.datetime.now().hour}:{datetime.datetime.now().minute.zfill(2)}"
+            update_df_with_appointment(
+                "appointments.csv",
+                CITY,
+                OFICINA,
+                "CUE",
             )
 
-        random_delay(MIN, MAX)
+            send_discord_message(WEBHOOK_URL, URL, logger)
 
     except Exception as e:
-        current_day = datetime.datetime.today().strftime("%A")
         logger.error(f"Script failed: {str(e)}")
+        current_day = datetime.datetime.today().strftime("%A")
+        with open(
+            f"error_page_{current_day}-{datetime.datetime.now().hour}-{str(datetime.datetime.now().minute).zfill(2)}.html",
+            "w",
+            encoding="utf-8",
+        ) as f:
+            f.write(driver.page_source)
         send_discord_message(
             WEBHOOK_URL,
-            f"Script failed on {current_day} at {datetime.datetime.now().hour}:{datetime.datetime.now().minute.zfill(2)}: {str(e)}",
+            f"Script failed on {current_day} at {datetime.datetime.now().hour}:{str(datetime.datetime.now().minute).zfill(2)}: {str(e)}",
             logger,
         )
 
@@ -244,8 +201,20 @@ def main(headless=True):
         driver.quit()
 
 
-if __name__ == "__main__":
+def main(headless=False):
     while True:
-        main()
-        logger.info("Sleeping for 20 minutes before the next execution.")
-        time.sleep(1200)
+        if is_within_time_ranges(TARGET_TIMES):
+            logger.info("Running in targeted mode...")
+            check_cue(headless)
+            next_interval = TARGETED_INTERVAL
+        else:
+            logger.info("Running in default mode!")
+            check_cue(headless)
+            next_interval = DEFAULT_INTERVAL
+
+        logger.info(f"Sleeping for {next_interval} minutes before the next execution.")
+        time.sleep(next_interval * 60)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils.py
@@ -0,0 +1,132 @@
+import os
+import json
+import time
+import random
+import requests
+import datetime
+
+import pandas as pd
+import undetected_chromedriver as uc
+
+from fake_useragent import UserAgent
+
+
+def update_json_with_current_time(json_file):
+    current_day = datetime.datetime.today().strftime("%A")
+    current_time = datetime.datetime.now()
+
+    with open(json_file, "r") as file:
+        data = json.load(file)
+
+    current_time_float = float(
+        f"{current_time.hour}.{str(current_time.minute).zfill(2)}"
+    )
+    data[current_day].append(float(current_time_float))
+
+    with open(json_file, "w") as file:
+        json.dump(data, file)
+
+
+def update_df_with_appointment(df_file, city, address, document):
+    now = datetime.datetime.now()
+    current_day = now.strftime("%A")  # Example: 'Monday'
+    current_date = now.strftime("%Y-%m-%d")  # Example: '2024-02-26'
+    current_time = now.strftime("%H:%M")
+
+    if os.path.exists(df_file):
+        df = pd.read_csv(df_file)
+    else:
+        columns = ["Date", "Day", "Time", "City", "Address", "Document"]
+        df = pd.DataFrame(columns=columns)
+
+    new_row = pd.DataFrame(
+        [[current_date, current_day, current_time, city, address, document]],
+        columns=df.columns,
+    )
+    df = pd.concat([df, new_row], ignore_index=True)
+
+    df.to_csv(df_file, index=False)
+
+
+def random_delay(min, max):
+    time.sleep(random.uniform(min, max))
+
+
+def send_discord_message(webhook_url, message, logger):
+    data = {
+        "content": message,
+    }
+    response = requests.post(webhook_url, json=data)
+    if response.status_code == 204:
+        logger.info("Message sent to Discord successfully.")
+    else:
+        logger.error(f"Failed to send message to Discord: {response.text}")
+
+
+def create_driver(headless=True):
+    options = uc.ChromeOptions()
+
+    if headless:
+        options.add_argument("--headless=new")
+        options.add_argument("--disable-gpu")
+
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--ignore-ssl-errors")
+    options.add_argument("--allow-insecure-localhost")
+    options.add_argument("--disable-web-security")
+
+    # Mimic regular browser behavior
+    options.add_argument("--start-maximized")
+
+    # adding argument to disable the AutomationControlled flag
+    options.add_argument("--disable-blink-features=AutomationControlled")
+
+    # options.add_experimental_option("useAutomatadd_experimental_optionionExtension", False)
+
+    # Use history data
+    home_directory = os.path.expanduser(
+        "~"
+    )  # Expands '~' to the full home directory path
+    user_data_dir = f"{home_directory}/.config/google-chrome/Default"
+    options.add_argument(f"--user-data-dir={user_data_dir}")
+    # options.add_argument("--user-data-dir=/tmp/selenium-profile")
+    # options.add_argument("--user-data-dir=~/.config/google-chrome/Default")
+
+    options.add_argument(f"--user-agent={UserAgent().random}")
+
+    options.add_argument("accept-language=es-ES,es;q=0.9,en;q=0.8")
+
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    return uc.Chrome(
+        headless=headless,
+        options=options,
+        driver_executable_path="/opt/chromedriver-linux64/chromedriver",
+        browser_executable_path="/usr/bin/google-chrome",
+        version_main=131,
+    )
+
+
+def is_within_time_ranges(time_ranges):
+    """
+    Check if current time is within any of the specified time ranges
+
+    Args:
+        time_ranges: List of tuples with (start_time, end_time) in format "HH:MM"
+
+    Returns:
+        bool: True if current time is within any of the ranges
+    """
+    current_time = datetime.datetime.now().time()
+
+    for start_time, end_time in time_ranges:
+        start = datetime.time(
+            int(start_time.split(":")[0]), int(start_time.split(":")[1])
+        )
+        end = datetime.time(int(end_time.split(":")[0]), int(end_time.split(":")[1]))
+
+        if start <= current_time <= end:
+            return True
+
+    return False
